042d4b366f5b700c44e4e3ee4cec6574e48a1296
valotr0n <beloshnez98@bk.ru>
2025-09-14 00:35:53 +0300

    first laba

---
+++ javafirstlab/differencIsEmptyIsBlank.java
@@ -0,0 +1,8 @@
+public class differencIsEmptyIsBlank {
+    public static void main(String[] args) {
+        String stringIsEmpty = "";
+        String stringIsBlank = "  ";
+        System.out.println(stringIsEmpty.isEmpty());
+        System.out.println(stringIsBlank.isBlank());
+    }
+}
